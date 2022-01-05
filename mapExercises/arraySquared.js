b104e8ee43f5dde84ae1645be830c8980cb5e6b5
Marianela Gambina <marianela.gambina@mulesoft.com>
2022-01-05 15:10:57 -0300

    init repo and first exercise problem

---
+++ mapExercises/arraySquared.js
@@ -0,0 +1,4 @@
+// Array squared
+// Square the value of every element in the array. Presume that you will only get numbers in the input array.
+// const input = [ 1, 2, 3, 4, 5 ];
+// result =  [ 1, 4, 9, 16, 25]
