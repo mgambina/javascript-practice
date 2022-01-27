671e6ef18ea4e0b0b2406648d39294d5be3f1c2b
Marianela Gambina <marianela.gambina@mulesoft.com>
2022-01-27 13:32:07 -0300

    improving code

--- cuaderno-de-escritura/nivel1-start-with.js
+++ cuaderno-de-escritura/nivel1-start-with.js
@@ -2,8 +2,9 @@
 // El texto debe tener 56 palabras
 // Controlar la cantidad de palabras, imprimir las palabras a arreglar y si cumple con todo, aprobado.
 
-const text = 'AME Esto siempre'
-const condition = ['A', 'E', 'P', 'D', 'S']
+const text = 'Pienso sobre empezar a escribir. Despacio. Parece simple pero es extraño. Escribir es desarmar pensamientos. Es encontrar algunas explicaciones. Permite distender el peso de entender algo desconocido. Primer paso: separar prejuicios propios. Segundo paso: dejar de pensar. Después: soltarse. Amerita explayarse para encontrar soluciones, para permutar, para pensarnos distintos. Permitirse discernir permanentemente para evolucionar. Pidamos ayuda.'
+const condition = ['A', 'E', 'P', 'D', 'S'];
+const requiredTextLength = 56;
 
 
 function wordsStartWith(text) {
@@ -26,11 +27,11 @@ function wordsStartWith(text) {
     console.log('The following words do not comply with the request', wordsToFix);
   }
 
-  if (wordsToFix.length === 0 && textToArray.length + 1 != 56) {
-    console.log(`Your text is not long enough. You still need ${56 - textToArray.length} words to meet the requirements.`)
-  }
-
-  if (wordsToFix.length === 0 && textToArray.length + 1 === 56) {
+  if (wordsToFix.length === 0 && textToArray.length < requiredTextLength) {
+    console.log(`Your text is not long enough. You still need ${requiredTextLength - textToArray.length} words to meet the requirements.`)
+  } else if (wordsToFix.length === 0 && textToArray.length > requiredTextLength) {
+    console.log(`Text is longer than what was requested`)
+  } else {
     console.log(`Your text is approved.`)
   }
 }
